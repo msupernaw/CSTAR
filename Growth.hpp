2d196a0f022a9cb7f875ad306d9312a988e1bd11
amart <zta@uw.edu>
2015-12-10 12:09:26 -0800

    Change user

--- Growth.hpp
+++ Growth.hpp
@@ -5,40 +5,71 @@
 
 namespace cstar {
 
+
     namespace growth {
 
         /**
+         * Logistic growth model.
          * 
-         * @param Linf - largest size (asymptote)
-         * @param a0   -
-         * @param a    - age
-         * @param k    - growth rate
+         * @param P0 - initial population size
+         * @param t  - time
+         * @param k  - relative growth rate
+         * @param K  - carrying capacity
+         * @param c  - harvest rate, default = 0
+         * 
          * @return 
          */
         template<typename T>
-        const T Bertalanffy(const T& Linf, const T& a0, const T& a, const T& k) {
-            return Linf * (1.0 - std::exp(-k * (a - a0)));
+        const T Logistic(const T& P0, const T& t, const T& k, const T& K, T c = T(0)) {
+            return K / (1.0 + ((K - P0) / P0) * std::exp(-k * t));
         }
 
-       /**
-         * Richards growth model
+        /**
+         * 
+         * @param Linf
+         * @param t0
+         * @param t
+         * @param K
+         * @return 
+         */
+        template<typename T>
+        const T Bertalanffy(const T& Linf, const T& t0, const T& t, const T& K) {
+            return Linf * (1.0 - std::exp(-K * (t - t0)));
+        }
+
+        /**
+         * Gompertz growth model
+         * 
+         * @param alpha - upper asymptote
+         * @param beta  - growth displacement
+         * @param t     - time 
+         * @param k     - growth rate
+         * @return 
+         */
+        template<typename T>
+        const T Gompertz(const T& alpha, const T& beta, const T& t, const T& k) {
+            return alpha * std::exp(-beta * exp(-k * t));
+        }
+
+        /**
+         * Richard growth model
          * 
          * @param alpha - upper asymptote
          * @param beta  - growth range
-         * @param a     - age
+         * @param t     - time
          * @param k     - growth rate
          * @param m     - slope
          * @return 
          */
         template<typename T>
-        const T Richards(const T& alpha, const T& beta, const T& a, const T& k, const T& m) {
-            return alpha /std::pow((1.0 + beta * std::exp(-k * a)), (1.0 / m));
+        const T Richard(const T& alpha, const T& beta, const T& t, const T& k, const T& m) {
+            return alpha /std::pow((1.0 + beta * std::exp(-k * t)), (1.0 / m));
         }
 
         /**
          * Schnute growth model
          * 
-         * @param a     - age
+         * @param t     - time
          * @param r0    - reference value
          * @param beta  - growth displacement
          * @param k     - growth rate
@@ -46,10 +77,11 @@ namespace cstar {
          * @return 
          */
         template<typename T>
-        const T Schnute(const T& a, const T& r0, const T& beta, const T& k, const T& m) {
-            return std::pow((r0 + beta * std::exp(k * a)), m);
+        const T Schnute(const T& t, const T& r0, const T& beta, const T& k, const T& m) {
+            return std::pow((r0 + beta * std::exp(k * t)), m);
         }
     }
+
 
 }
 
