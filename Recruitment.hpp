2e921490f00f69a843ad09144d4a3ac986ae6c77
amart <zta@uw.edu>
2015-12-11 11:54:40 -0800

    Add alt Ricker

--- Recruitment.hpp
+++ Recruitment.hpp
@@ -22,14 +22,15 @@ namespace cstar {
         /**
          * Alternative Ricker S-R relationship
          *
-         * @param alpha -
-         * @param beta  -
-         * @param S     - spawning biomass or abundance
+         * @param R0   - unfished equilibrium recruitment
+         * @param phi0 - unfished spawning biomass or abundance per recruit
+         * @param A    - adjustment factor
+         * @param S    - spawning biomass or abundance
          * @return
          */
         template<typename T>
-        const T Ricker_alt(const T& alpha, const T& beta, const T& S) {
-            return alpha * S * std::exp(-1.0 * beta * S);
+        const T Ricker_alt(const T& R0, const T& phi0, const T& A, const T& S) {
+            return (S / phi0) * std::exp(A * (1.0 - (S / (phi0 * R0))));
         }
 
         /**
@@ -48,14 +49,15 @@ namespace cstar {
         /**
          * Alternative Beverton-Holt S-R relationship
          *
-         * @param alpha  - maximum recruitment 
-         * @param beta   - the stock level needed to produce the half of maximum recruitment
-         * @param S      - spawning biomass or abundance
+         * @param R0   - unfished equilibrium recruitment
+         * @param phi0 - unfished spawning biomass or abundance per recruit
+         * @param h    - steepness
+         * @param S    - spawning biomass or abundance
          * @return 
          */
         template<typename T>
-        const T BevertonHolt_alt(const T& alpha, const T& beta, const T& S) {
-            return (alpha * S) / (beta + S);
+        const T BevertonHolt_alt(const T& R0, const T& phi0, const T& h, const T& S) {
+            return (4.0 * R0 * h * S) / ((phi0 * R0 * (1.0 - h)) + (S * (5.0 * h - 1.0)));
         }
 
         /**
@@ -90,10 +92,10 @@ namespace cstar {
         /**
          * Deriso S-R relationship
          * 
-         * @param alpha
-         * @param beta
-         * @param c
-         * @param S
+         * @param alpha -
+         * @param beta  -
+         * @param c     - exponent
+         * @param S     - spawning biomass or abundance
          * @return 
          */
         template<typename T>
